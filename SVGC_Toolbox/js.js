4f0213b9be62cc80f49e97ff7e68185f0409a222
julio <halflifemen@gmail.com>
2018-12-17 08:41:54 -0430

    v0.0.2
    - modificasión de la estructura del formulario frente a los cambios de resolucion 13/12/2018

--- SVGC_Toolbox/js.js
+++ SVGC_Toolbox/js.js
@@ -3,6 +3,7 @@
         doc[i].style
 		.height =$('.head').height()+"px";
 	}*/
+var isresise=true;
 window.onload = function() {
 
 	var h=$(window).height();
@@ -12,7 +13,7 @@ window.onload = function() {
 		.height =(h*0.8)+"px";
 	document.getElementsByClassName("consult")[0]
 		.style
-		.height =(h*0.8*5)+"px";
+		.height =(h*0.8)+"px";
 	document.getElementsByClassName("space")[0]
 	    .style
 		.height =$('.head').height()+"px";
@@ -37,21 +38,29 @@ window.addEventListener('resize',
 		.height =(h*0.8)+"px";
 	document.getElementsByClassName("consult")[0]
 		.style
-		.height =(h*0.8*5)+"px";
+		.height =(h*0.8)+"px";
 	
 
-		if ( $(window).width() > 790) {      
+		if ( $(window).width() > 790) {  
+			isresise=true;
+    
 			var formulario = document.getElementsByClassName("DivForm2");
 			var tabbla = document.getElementsByClassName("consult");
 
-			tabbla[0].style.display = "inherit"
+			tabbla[0].style.display = "flex"
 			formulario[0].style.display = "inherit";
 		} else {
-			var formulario = document.getElementsByClassName("DivForm2");
-			var tabbla = document.getElementsByClassName("consult");
 
-			tabbla[0].style.display = "inherit"
-			formulario[0].style.display = "none";
+			if(isresise==true){
+				var formulario = document.getElementsByClassName("DivForm2");
+				var tabbla = document.getElementsByClassName("consult");
+
+				tabbla[0].style.display = "flex"
+				formulario[0].style.display = "none";
+			}
+			
+			isresise=false;
+
 		}
 	}
 );
